bbecbc75d5023110f4541e3bb6dff81404c90c75
Ibrohim <fozilovic007@gmail.com>
2025-03-18 18:43:17 +0500

    Allowed host

--- Config/settings.py
+++ Config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['165.227.130.229','127.0.0.1']
 
 
 # Application definition
